a9ecbbcce601794c516e0357fb96e0393c99b3d4
sitranto <89784211+sitranto@users.noreply.github.com>
2025-10-08 17:34:47 +0300

    Added resources and form data initialization

--- Arkanoid/MainForm.cs
+++ Arkanoid/MainForm.cs
@@ -5,6 +5,17 @@ namespace Arkanoid
         public MainForm()
         {
             InitializeComponent();
+            InitializeFormData();
+        }
+
+        public void InitializeFormData()
+        {
+            FormBorderStyle = FormBorderStyle.None;
+            Size = Screen.PrimaryScreen!.Bounds.Size;
+            BackgroundImage = Properties.Resources.arkanoid_bg;
+            DoubleBuffered = true;
+
+            KeyDown += (_, _) => { Close(); };
         }
     }
 }

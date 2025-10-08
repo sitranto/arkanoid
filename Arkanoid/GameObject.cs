615d6b06693b93edbe1166e970ed0b9378123c01
sitranto <89784211+sitranto@users.noreply.github.com>
2025-10-08 18:28:54 +0300

    Added platform moving logic

---
+++ Arkanoid/GameObject.cs
@@ -0,0 +1,19 @@
+﻿namespace Arkanoid
+{
+    /// <summary>
+    /// Абстрактный класс, являющийся общим для всех игровых объектов.
+    /// </summary>
+    public abstract class GameObject
+    {
+        public float X {  get; set; }
+        public float Y { get; set; }
+        public float Width { get; set; }
+        public float Height { get; set; }
+
+        /// <summary>
+        /// Метод отрисовки игрового объекта.
+        /// </summary>
+        /// <param name="g">Объект типа Graphics, который будет использован для отрисовки.</param>
+        public abstract void Draw(Graphics g);
+    }
+}
--- Arkanoid/MainForm.cs
+++ Arkanoid/MainForm.cs
@@ -2,20 +2,61 @@ namespace Arkanoid
 {
     public partial class MainForm : Form
     {
+        private List<GameObject> gameObjects = [];
+        private Platform? platform;
+        private Rectangle screenBounds = Screen.PrimaryScreen!.Bounds;
+
         public MainForm()
         {
             InitializeComponent();
             InitializeFormData();
+            InitializeGameData();
         }
 
-        public void InitializeFormData()
+        /// <summary>
+        /// Метод инициализации основных свойств формы.
+        /// </summary>
+        private void InitializeFormData()
         {
             FormBorderStyle = FormBorderStyle.None;
-            Size = Screen.PrimaryScreen!.Bounds.Size;
+            Size = screenBounds.Size;
             BackgroundImage = Properties.Resources.arkanoid_bg;
             DoubleBuffered = true;
 
             KeyDown += (_, _) => { Close(); };
         }
-    }
+
+        /// <summary>
+        /// Метод инициализации игровых объектов и привязки игровых функций к обработчикам событий формы.
+        /// </summary>
+        private void InitializeGameData()
+        {
+            platform = new Platform(200, screenBounds.Height - 50, screenBounds.Width / 10, screenBounds.Height / 50);
+            gameObjects.Add(platform);
+
+            MouseMove += OnMouseMove;
+        }
+
+        /// <summary>
+        /// Функция привязки объекта <see cref="Platform"/> к движению мыши.
+        /// </summary>
+        private void OnMouseMove(object? sender, MouseEventArgs e)
+        {
+            float newX = e.X - platform!.Width / 2f;
+            newX = Math.Max(0, Math.Min(newX, ClientSize.Width - platform.Width));
+            platform.X = newX;
+
+            Invalidate();
+        }
+
+        protected override void OnPaint(PaintEventArgs e)
+        {
+            base.OnPaint(e);
+
+            foreach (var gameObject in gameObjects)
+            {
+                gameObject.Draw(e.Graphics);
+            }
+        }
+}
 }
